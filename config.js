f4b1c5c3b690b06c48043cab6328c5916d430705
Lu Wang <loowang@hotmail.com>
2020-08-21 11:06:24 -0400

    fix: db security

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const pkg = require('./package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 module.exports = {
-  dialect: 'postgres',
+  dialect: process.env.DIALECT,
   logging: false,
-  host: process.env.DATABASE_URL || 'capstone.c3dxjld76vva.us-east-1.rds.amazonaws.com',
-  password: process.env.DB_PASSWORD || 'capstoneTeam#1',
-  username: process.env.DB_USERNAME || 'capstone',
-  port: '5432',
+  host: process.env.DATABASE_URL,
+  password: process.env.DB_PASSWORD,
+  username: process.env.DB_USERNAME,
+  port: process.env.PORT,
   database: databaseName
 }
