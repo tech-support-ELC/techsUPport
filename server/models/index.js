345c3734150f8cbf772c0cc9984db73188cde408
Biligma Oyola <sandylykova94@mail.ru>
2020-08-21 06:48:18 -0400

    feat:condition model, patient_condition model, index.js update

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const Condition = require('./condition');
+const Patient_Condition = require('./patient_condition');
+
+module.exports = {
+  Condition,
+  Patient_Condition
+};
---
+++ server/models/patient_condition.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+// const db = require('./db')
+
+const Patient_Condition = db.define('patient_condition', {
+  value: {
+    type: Sequelize.INTEGER,
+  },
+  date: {
+    type: Sequelize.DATE,
+  },
+  notes: {
+    type: Sequelize.TEXT
+  }
+});
+
+module.exports = Patient_Condition;
