6218f9b6c77fbb1ba5c7a9fd10351f7bfd587f72
esk <emseely@gmail.com>
2020-08-24 10:55:40 -0400

    chore: config

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
-const pkg = require('./package.json')
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-if (process.env.NODE_ENV !== 'production') require('./dbSecrets')
+const pkg = require("./package.json");
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
+if (process.env.NODE_ENV !== "production") require("./dbSecrets");
 
 module.exports = {
-  dialect: process.env.DIALECT,
+  dialect: "postgres",
   logging: false,
   host: process.env.DATABASE_URL,
   password: process.env.DB_PASSWORD,
   username: process.env.DB_USERNAME,
-  port: process.env.PORT,
-  database: databaseName
-}
+  port: "5432",
+  database: databaseName,
+};
