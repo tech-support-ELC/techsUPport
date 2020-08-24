b626b492b2bf2914c98f3da0afdbe33ddd178cfc
Lu Wang <loowang@hotmail.com>
2020-08-24 15:10:23 -0400

    feat: add logout

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { logout } from '../redux/auth'
 
 const Home = (props) => {
   const { firstName } = props.currentUser
+  const { handleClick } = props
   return (
     <div>
       <h1>Welcome {firstName}!</h1>
       <h2>Onboarding</h2>
-    </div>
+      <p>
+        <button type='submit' onClick={handleClick}>Log out</button>
+      </p>
+    </div >
   )
 }
 const mapState = ({ currentUser }) => ({ currentUser })
 
-export default connect(mapState)(Home)
+const mapDispatch = (dispatch, ownProps) => ({
+  handleClick: () => {
+    dispatch(logout(ownProps.history))
+  }
+})
+
+export default connect(mapState, mapDispatch)(Home)
