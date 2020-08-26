051c08d72b9ee31217153b749b4c2d2e3a96ec0f
esk <emseely@gmail.com>
2020-08-26 11:03:39 -0400

    fix: scss import

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import store from './store'
-import Root from './components/Root'
+import store from "./store";
+import Root from "./components/Root";
 import Footer from "./components/Footer";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
+import "./style.scss";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,8 +14,9 @@ ReactDOM.render(
       <Root />
       <Footer />
     </Router>
-  </Provider>, document.getElementById('root'));
-
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
