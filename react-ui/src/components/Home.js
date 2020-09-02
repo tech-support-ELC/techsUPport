027e5b4faddc309adf0b3dcdcefe82da9491fd65
Megan Leaf <meganbleaf@gmail.com>
2020-09-01 23:00:08 -0400

    chore: moved add buttons for home page into a new component

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -13,46 +13,12 @@ import AddMedication from '../components/AddMedication'
 import { fetchMedications } from "../redux/medications";
 import { getChartThunk } from '../redux/score'
 import home from '../images/home.png'
+import HomeAddButtons from './HomeAddButtons'
 
 
 export class Home extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      showDoctorModal: false,
-      showConditionModal: false,
-      showMedicineModal: false
-    }
-    this.openDoctorModal = this.openDoctorModal.bind(this)
-    this.openConditionModal = this.openConditionModal.bind(this)
-    this.openMedicineModal = this.openMedicineModal.bind(this)
-    this.closeDoctorModal = this.closeDoctorModal.bind(this)
-    this.closeConditionModal = this.closeConditionModal.bind(this)
-    this.closeMedicineModal = this.closeMedicineModal.bind(this)
-  }
-
-  openDoctorModal() {
-    this.setState({ showDoctorModal: true })
-  }
-  openConditionModal() {
-    this.setState({ showConditionModal: true })
-  }
-  openMedicineModal() {
-    this.setState({ showMedicineModal: true })
-  }
-
-  closeDoctorModal() {
-    this.setState({ showDoctorModal: false })
-  }
-  closeConditionModal() {
-    this.setState({ showConditionModal: false })
-  }
-  closeMedicineModal() {
-    this.setState({ showMedicineModal: false })
-  }
 
   componentDidMount() {
-    ReactModal.setAppElement("body")
     this.props.getAllDoctors()
     this.props.getAppointments()
     this.props.getAllConditions()
@@ -72,66 +38,25 @@ export class Home extends React.Component {
         <h1>Welcome {firstName}!</h1>
         <div>
           {
-            (doctors.length === 0 || conditions.length === 0 || medications.length === 0) ?
+            (doctors.length === 0 && conditions.length === 0 && medications.length === 0) ?
               (
-                <div>
-                  <h2>Get started by adding your doctors, conditions, and medications</h2>
-                  <>
-                    <img src={home} alt="" />
-                  </>
-                  {/* <div>
-                    <button onClick={this.openDoctorModal}>Add a Doctor</button>
-                    <ReactModal isOpen={this.state.showDoctorModal} contentLabel="Example Modal">
-                      <AddDoctor
-                        currentUser={this.props.currentUser}
-                        addNewDoctor={this.props.addNewDoctor}
-                      />
-                      <button onClick={this.closeDoctorModal}>Close</button>
-                    </ReactModal>
-                  </div>
-
-                  <div>
-                    <button type="button" onClick={this.openMedicineModal}>Add a Medication</button>
-                    <ReactModal isOpen={this.state.showMedicineModal} contentLabel="Single Document">
-                      <>
-                        <AddMedication />
-                        <button type="button" onClick={this.closeMedicineModal}>Close</button>
-                      </>
-                    </ReactModal>
-                  </div>
-
-                  <div>
-                    <button onClick={this.openConditionModal}>Add a Condition</button>
-                    <ReactModal isOpen={this.state.showConditionModal} contentLabel="Single Document">
-                      <AddConditionForm
-                        currentUser={this.props.currentUser}
-                        addCondition={this.props.addCondition}
-                      />
-                      <button onClick={this.closeConditionModal}>close</button>
-                    </ReactModal>
-                  </div> */}
-
-
-                </div>
-              )
-              : (doctors && doctors.length > 0 && appointments && appointments.length > 0) ?
-                <DoctorDonut appointment={appointments} doctors={doctors} /> :
-                (chart && chart.length > 0) ?
-                  <LineChart />
-                  : null
+                <h2>Get started by adding your doctors, conditions, and medications</h2>
+              ) : null
           }
+          <HomeAddButtons />
+          <>
+            <img src={home} alt="" />
+          </>
         </div>
-        <div>
-          {
+        {
+          (doctors && doctors.length > 0 && appointments && appointments.length > 0) ?
+            <DoctorDonut appointment={appointments} doctors={doctors} /> :
+            (chart && chart.length > 0) ?
+              <LineChart />
+              : null
+        }
+      </div>
 
-          }
-        </div>
-        <div>
-          {
-          }
-        </div>
-
-      </div >
     )
   }
 }
---
+++ react-ui/src/components/HomeAddButtons.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { getAppointmentThunk } from '../redux/dcDoctor'
+import { getAllDoctorsThunk, addDoctorThunk } from '../redux/doctors'
+import { getAllConditionsThunk, addConditionThunk } from '../redux/conditions'
+import ReactModal from "react-modal"
+import AddDoctor from '../components/AddDoctor'
+import AddConditionForm from '../components/AddConditionForm'
+import AddMedication from '../components/AddMedication'
+import { fetchMedications } from "../redux/medications";
+import { getChartThunk } from '../redux/score'
+
+
+
+export class HomeAddButtons extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            showDoctorModal: false,
+            showConditionModal: false,
+            showMedicineModal: false
+        }
+        this.openDoctorModal = this.openDoctorModal.bind(this)
+        this.openConditionModal = this.openConditionModal.bind(this)
+        this.openMedicineModal = this.openMedicineModal.bind(this)
+        this.closeDoctorModal = this.closeDoctorModal.bind(this)
+        this.closeConditionModal = this.closeConditionModal.bind(this)
+        this.closeMedicineModal = this.closeMedicineModal.bind(this)
+    }
+
+    openDoctorModal() {
+        this.setState({ showDoctorModal: true })
+    }
+    openConditionModal() {
+        this.setState({ showConditionModal: true })
+    }
+    openMedicineModal() {
+        this.setState({ showMedicineModal: true })
+    }
+
+    closeDoctorModal() {
+        this.setState({ showDoctorModal: false })
+    }
+    closeConditionModal() {
+        this.setState({ showConditionModal: false })
+    }
+    closeMedicineModal() {
+        this.setState({ showMedicineModal: false })
+    }
+
+    componentDidMount() {
+        ReactModal.setAppElement("body")
+        this.props.getAllDoctors()
+        this.props.getAppointments()
+        this.props.getAllConditions()
+        this.props.getMedications()
+        this.props.getChart()
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <button onClick={this.openDoctorModal}>Add a Doctor</button>
+                    <ReactModal isOpen={this.state.showDoctorModal} contentLabel="Example Modal">
+                        <AddDoctor
+                            currentUser={this.props.currentUser}
+                            addNewDoctor={this.props.addNewDoctor}
+                        />
+                        <button onClick={this.closeDoctorModal}>Close</button>
+                    </ReactModal>
+                </div>
+
+                <div>
+                    <button type="button" onClick={this.openMedicineModal}>Add a Medication</button>
+                    <ReactModal isOpen={this.state.showMedicineModal} contentLabel="Single Document">
+                        <>
+                            <AddMedication />
+                            <button type="button" onClick={this.closeMedicineModal}>Close</button>
+                        </>
+                    </ReactModal>
+                </div>
+
+                <div>
+                    <button onClick={this.openConditionModal}>Add a Condition</button>
+                    <ReactModal isOpen={this.state.showConditionModal} contentLabel="Single Document">
+                        <AddConditionForm
+                            currentUser={this.props.currentUser}
+                            addCondition={this.props.addCondition}
+                        />
+                        <button onClick={this.closeConditionModal}>close</button>
+                    </ReactModal>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+
+const mapDispatch = (dispatch) => ({
+    getAllDoctors: () => dispatch(getAllDoctorsThunk()),
+    getAppointments: () => dispatch(getAppointmentThunk()),
+    getAllConditions: () => dispatch(getAllConditionsThunk()),
+    addCondition: (condition) => dispatch(addConditionThunk(condition)),
+    addNewDoctor: (newDoctor) => dispatch(addDoctorThunk(newDoctor)),
+    getMedications: () => dispatch(fetchMedications()),
+    getChart: () => dispatch(getChartThunk())
+})
+
+export default connect(null, mapDispatch)(HomeAddButtons)
