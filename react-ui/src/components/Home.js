c2a45ef8ee11b04af4d3da79215a023cd1473627
esk <emseely@gmail.com>
2020-09-03 13:36:27 -0700

    style: home page

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -6,25 +6,24 @@ import { getAllDoctorsThunk, addDoctorThunk } from "../redux/doctors";
 import { getAllConditionsThunk, addConditionThunk } from "../redux/conditions";
 import LineChart from "./lineChart/LineChartCondition";
 import { fetchMedications } from "../redux/medications";
-import { getChartThunk } from '../redux/score'
-import home from '../images/home.png'
-import HomeAddButtons from './HomeAddButtons'
-import moment from 'moment'
-import { Link } from 'react-router-dom'
-import ReactModal from 'react-modal'
-import Onboarding from './Onboarding'
-import checkDay from '../utils/onboarding-date-function'
+import { getChartThunk } from "../redux/score";
+import home from "../images/home.png";
+import HomeAddButtons from "./HomeAddButtons";
+import moment from "moment";
+import { Link } from "react-router-dom";
+import ReactModal from "react-modal";
+import Onboarding from "./Onboarding";
+import checkDay from "../utils/onboarding-date-function";
 
-import CalendarHeatmap from 'react-calendar-heatmap';
-import 'react-calendar-heatmap/dist/styles.css';
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
 
 export class Home extends React.Component {
   constructor() {
     super();
     this.state = {
-
       data: [],
-    }
+    };
   }
   async componentDidMount() {
     await this.props.getChart();
@@ -33,50 +32,47 @@ export class Home extends React.Component {
     this.props.getAllConditions();
     this.props.getMedications();
 
-    const count = this.props.chart.map(eachScore => eachScore.rate);
-    const date = this.props.chart.map(eachDate => eachDate.date);
+    const count = this.props.chart.map((eachScore) => eachScore.rate);
+    const date = this.props.chart.map((eachDate) => eachDate.date);
     this.setState((prevState) => {
       const data = date.map((d, i) => ({
         date: d,
-        count: count[i]
-      }))
+        count: count[i],
+      }));
       return {
-        data
-      }
-    })
+        data,
+      };
+    });
   }
   render() {
-    const { firstName } = this.props.currentUser
-    const appointments = this.props.appointment
-    const doctors = this.props.doctors
-    const conditions = this.props.conditions
-    const medications = this.props.medications
-    const currentUser = this.props.currentUser
+    const { firstName } = this.props.currentUser;
+    const appointments = this.props.appointment;
+    const doctors = this.props.doctors;
+    const conditions = this.props.conditions;
+    const medications = this.props.medications;
+    const currentUser = this.props.currentUser;
     const chart = this.props.chart;
     const data = this.state.data;
-    console.log(data)
+    console.log(data);
     return (
       <div className="home">
         <div>
           <h1 id="welcomeName">Welcome, {firstName}!</h1>
           {!checkDay(currentUser.createdAt) ? <Onboarding /> : null}
+        </div>
+        <div>
           {doctors.length === 0 &&
-            conditions.length === 0 &&
-            medications.length === 0 ? (
-              <h2>
-                Get started by adding your doctors, conditions, and medications
-                <div className="mainHomepageArea">
-                  <img src={home} alt="" />
-                </div>
-              </h2>
-
-            ) : null}
+          conditions.length === 0 &&
+          medications.length === 0 ? (
+            <h2>
+              Get started by adding your doctors, conditions, and medications
+            </h2>
+          ) : null}
           <h2>
             Fill out your daily check-in for {moment().format("MMMM Do YYYY")}
           </h2>
           <div id="dailyCheckinHomePage">
             <Link to="/dailycheckin">
-
               <button id="checkin">
                 <span>Daily Check-in</span>
               </button>
@@ -86,27 +82,31 @@ export class Home extends React.Component {
           <HomeAddButtons />
         </div>
         <div className="mainHomepageArea">
-          {/* {((!doctors || doctors.length === 0) && (!conditions || conditions.length === 0) && (!medications && medications.length === 0)) ?
-            <img src={home} alt="" /> : null
-          } */}
-          {
-            (doctors && doctors.length > 0 && appointments && appointments.length > 0) ?
-              <DoctorDonut appointment={appointments} doctors={doctors} /> : null
-          }
-          {
-            (chart && chart.length > 0) ?
-              <div>
-                <h4>My conditions over time</h4>
-                <CalendarHeatmap
-                  values={data}
-                  classForValue={(value) => {
-                    if (!value) {
-                      return 'color-empty';
-                    }
-                    return `color-scale-${value.count}`;
-                  }}
-                /> </div> : null
-          }
+          {doctors.length === 0 &&
+          conditions.length === 0 &&
+          medications.length === 0 ? (
+            <img src={home} alt="" />
+          ) : null}
+          {doctors &&
+          doctors.length > 0 &&
+          appointments &&
+          appointments.length > 0 ? (
+            <DoctorDonut appointment={appointments} doctors={doctors} />
+          ) : null}
+          {chart && chart.length > 0 ? (
+            <div>
+              <h4>My conditions over time</h4>
+              <CalendarHeatmap
+                values={data}
+                classForValue={(value) => {
+                  if (!value) {
+                    return "color-empty";
+                  }
+                  return `color-scale-${value.count}`;
+                }}
+              />{" "}
+            </div>
+          ) : null}
         </div>
       </div>
     );
