e8ee9f53fd7122287fcd945c8644448421e5fc08
Lu Wang <loowang@hotmail.com>
2020-09-06 19:12:49 -0400

    fix: add id in document model

--- server/db/models/document.js
+++ server/db/models/document.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Document = db.define('documents', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   description: {
     type: Sequelize.STRING,
     defaultValue: 'Undefined',
