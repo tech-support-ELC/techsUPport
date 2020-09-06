62152a93006b214e1dbab2540d267ce949ad265b
Biligma Oyola <sandylykova94@mail.ru>
2020-09-06 11:33:16 -0400

    feat: conditions bar chart

--- react-ui/src/components/datavis/BarChart.js
+++ react-ui/src/components/datavis/BarChart.js
@@ -36,19 +36,7 @@ class BarChart extends React.Component {
         data,
       };
     });
-}
-  // state = {
-  //   data: [
-  //     { name: 'Mon', value: 20 },
-  //     { name: 'Tue', value: 40 },
-  //     { name: 'Wed', value: 35 },
-  //     { name: 'Thu', value: 50 },
-  //     { name: 'Fri', value: 55 },
-  //     { name: 'Sat', value: 40 },
-  //     { name: 'Sun', value: 30 }
-  //   ]
-  // }
-
+  }
   render() {
     let barHeight = 30
 
